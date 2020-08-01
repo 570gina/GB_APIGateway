411fcb41bfe5034be6bc87c1f51497f5d42973a6
570gina <570gina@gmail.com>
2020-08-01 23:49:33 +0800

    An API Gateway service for Green Button System

---
+++ src/test/java/com/greenbutton/api/gateway/GbApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greenbutton.api.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GbApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
